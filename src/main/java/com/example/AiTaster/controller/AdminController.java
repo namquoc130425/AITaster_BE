406bc71ba64d3efe6b72fc1585cff570a2515558
Nguyen Vuong Quoc Nam <namquoc725@gmail.com>
2026-05-30 09:49:32 +0700

    feat: add admin user management

--- src/main/java/com/example/AiTaster/controller/AdminController.java
+++ src/main/java/com/example/AiTaster/controller/AdminController.java
@@ -2,9 +2,12 @@ package com.example.AiTaster.controller;
 
 import com.example.AiTaster.dto.UserResponse;
 import com.example.AiTaster.dto.request.AdminRequest;
+import com.example.AiTaster.dto.response.APIResponse;
+import com.example.AiTaster.dto.response.ClientProfileResponse;
 import com.example.AiTaster.service.AdminService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,41 +23,44 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping
-    public List<UserResponse> getAllUsers() {
-        return adminService.getAllUsers();
+    public ResponseEntity<APIResponse<List<UserResponse>>> getAllUsers() {
+        List<UserResponse> responses = adminService.getAllUsers();
+        return ResponseEntity.ok(APIResponse.response(200, "Get all users successfully", responses));
     }
 
     @GetMapping("/{userId}")
-    public UserResponse getUserById(@PathVariable Long userId) {
-        return adminService.getUserById(userId);
+    public  ResponseEntity<APIResponse<UserResponse>> getUserById(@PathVariable Long userId) {
+        UserResponse response = adminService.getUserById(userId);
+        return ResponseEntity.ok(APIResponse.response(200, "Get user successfully", response)) ;
     }
 
     @PostMapping
-    public UserResponse createUser(@RequestBody AdminRequest request) {
-        return adminService.createUser(request);
+    public ResponseEntity<APIResponse<UserResponse>> createUser(@RequestBody AdminRequest request) {
+        UserResponse userResponse = adminService.createUser(request);
+        return ResponseEntity.ok(APIResponse.response(200,"Create user successfully" ,userResponse));
     }
 
     @PutMapping("/{userId}")
-    public UserResponse updateUser(
-            @PathVariable Long userId,
-            @RequestBody AdminRequest request
-    ) {
-        return adminService.updateUser(userId, request);
+    public ResponseEntity<APIResponse<UserResponse>> updateUser(@PathVariable Long userId, @RequestBody AdminRequest request) {
+        UserResponse userResponse = adminService.updateUser(userId,request);
+        return ResponseEntity.ok(APIResponse.response(200,"Update user successfully",userResponse));
     }
 
     @PatchMapping("/{userId}/ban")
-    public UserResponse banUser(@PathVariable Long userId) {
-        return adminService.banUser(userId);
+    public ResponseEntity<APIResponse<UserResponse>>banUser(@PathVariable Long userId) {
+        UserResponse userResponse = adminService.banUser(userId);
+        return ResponseEntity.ok(APIResponse.response(200,"Ban user successfully",userResponse));
     }
 
     @PatchMapping("/{userId}/activate")
-    public UserResponse activateUser(@PathVariable Long userId) {
-        return adminService.activateUser(userId);
+    public ResponseEntity<APIResponse<UserResponse>> activateUser(@PathVariable Long userId) {
+        UserResponse userResponse = adminService.activateUser(userId);
+        return ResponseEntity.ok(APIResponse.response(200,"Activate user successfully",userResponse));
     }
 
     @DeleteMapping("/{userId}")
-    public String deleteUser(@PathVariable Long userId) {
+    public ResponseEntity<APIResponse<UserResponse>> deleteUser(@PathVariable Long userId) {
         adminService.deleteUser(userId);
-        return "User deleted successfully";
+        return ResponseEntity.ok(APIResponse.response(200,"Delete user successfully",null));
     }
 }
