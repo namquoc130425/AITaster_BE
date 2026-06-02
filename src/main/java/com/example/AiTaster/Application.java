5b55424d571100e109acd2fb8a68400043ea7ef9
Nguyen Vuong Quoc Nam <namquoc725@gmail.com>
2026-06-02 20:59:44 +0700

    backup jobpost before merge main

--- src/main/java/com/example/AiTaster/Application.java
+++ src/main/java/com/example/AiTaster/Application.java
@@ -10,18 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure()
-				.ignoreIfMissing()
-				.load();
-
-		String geminiKey = dotenv.get("GEMINI_API_KEY");
-
-		if (geminiKey != null) {
-			System.setProperty("GEMINI_API_KEY", geminiKey);
-			System.out.println("Gemini API key loaded: " + geminiKey.substring(0, 8) + "...");
-		} else {
-			System.out.println("Gemini API key NOT FOUND");
-		}
 		SpringApplication.run(Application.class, args);
 	}
 
