bc6c102db7aa69d77c62009428ba6dae57b49d5a
Nguyen Vuong Quoc Nam <namquoc725@gmail.com>
2026-07-06 14:39:25 +0700

    fix category-skill

--- src/main/java/com/example/AiTaster/dto/response/ExpertProfileResponse.java
+++ src/main/java/com/example/AiTaster/dto/response/ExpertProfileResponse.java
@@ -1,6 +1,8 @@
 package com.example.AiTaster.dto.response;
 
 import com.example.AiTaster.dto.UserResponse;
+import com.example.AiTaster.entity.Category;
+import com.example.AiTaster.entity.Skill;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,7 @@ import lombok.experimental.FieldDefaults;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -19,8 +22,8 @@ import java.time.LocalDateTime;
 public class ExpertProfileResponse {
     Long expertProfileId;
     String bio;
-    String category;
-    String skills;
+    Category category;
+    List<Skill> skills;
     Integer yearOfExperience;
     String portfolioUrl;
     BigDecimal rating;
